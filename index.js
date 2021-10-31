248bc3134fb19057f846b2b2d3a8bed0ce1f468f
harsh6975 <harsh.sinha@iitg.ac.in>
2021-10-31 14:32:05 +0530

    change

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
+const http = require("http");
 const express = require("express");
 const route = require("./router/indexRouter");
-const port = 3000;
+const socketio = require("socket.io");
 
 const app = express();
+const chatServer = http.createServer(app);
+const io = socketio(chatServer);
+
+const port = 3000;
 
 app.use(express.static("./assets"));
 app.set("view engine", "ejs");
 app.set("views", "./views");
+
+io.on("connection", (socket) => {
+  console.log("chat server connected");
+});
 
 app.get("/", route);
 
